f70bb958f1dc8c8cc2616ba1d23915c637b6e8cb
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-06-08 12:23:23 +0200

    Create 05_move_rename_operations.py
    Basic concepts for moving and renaming files within the filesystem of the operation system.

---
+++ 05_move_rename_operations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Python 3.9.7
+
+# 05_move_rename_operations.py
+
+# Dependencies
+import os, shutil
+
+abs_dir = 'C:\\Users\\name'
+abs_target_dir = 'C:\\Users\\name\\someDir'
+
+file_name = 'file.txt'
+new_file_name = 'new_file.txt'
+
+# Move operation
+os.chdir(abs_dir) # Change working directory
+shutil.move(file_name, abs_target_dir) # Move file to directory
+
+# Rename operation
+os.chdir(abs_target_dir) # Change working directory
+shutil.move(file_name, new_file_name) # Rename file name.
