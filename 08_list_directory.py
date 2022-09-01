a7b3c44b6f4f7ba9b851d2ffe8196555b535a3ba
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-09-01 23:34:09 +0200

    Update 08_list_directory.py

--- 08_list_directory.py
+++ 08_list_directory.py
@@ -4,6 +4,8 @@
 
 # 08_list_directory.py
 
+# Purpose: Get the file names and sizes similar to the dir or ls command.
+
 # Dependency
 import os
 
@@ -12,5 +14,10 @@ path = '/Users/user/directory'
 files = os.listdir(path)
 files.sort()
 
-for name in files:
-  print(name)
+total_size_bytes = 0
+for file_name in os.listdir(path):
+    file_size_bytes = os.path.getsize(os.path.join(path, file_name))
+    print("File name:", file_name, "File size:", file_size_bytes, "Byte")
+    total_size_bytes = total_size_bytes + file_size_bytes
+
+print("Total size:", total_size_bytes, "Bytes")
