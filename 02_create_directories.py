b7082b52bfb87fb1c431ecd3a884e7bf405412ed
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-09-06 08:27:25 +0200

    Update 02_create_directories.py

--- 02_create_directories.py
+++ 02_create_directories.py
@@ -8,23 +8,23 @@
 import os
 from pathlib import Path
 
-def create_directories():
+def create_directories(description):
     subfolder_name:str = ''
-    subfolder:str = '_Description'
     strDirectoryNum:str = ''
 
     for intCounter in range(1, 15):
         if intCounter < 10:
-            strDirectoryNum = '0' + str(intCounter)
+            strDirectoryNum = '0' + str(intCounter) 
         else:
             strDirectoryNum = str(intCounter)
 
-        subfolder_name = strDirectoryNum + subfolder
+        subfolder_name = strDirectoryNum + description + '_' + str(intCounter)
         if not os.path.exists(subfolder_name):
             os.makedirs(subfolder_name)
 
 if __name__ == "__main__":
-    p = 'C:\\...'
+    p = 'C:\\Users\\...\\'
     os.chdir(p) # Change current directory
     Path.cwd()  # Show the current directory path
-    create_directories()
+    description:str = '_Chapter'
+    create_directories(description)
