c514b0538621ef1986f902e3e4a72bdb90a268ad
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-09-17 11:59:38 +0200

    Update 11_import_file.py

--- 11_import_file.py
+++ 11_import_file.py
@@ -4,13 +4,18 @@
 
 # 11_import_file.py
 
+# Basic concept of importing a file, e.g. a TXT file.
+
 # Dependency
 import os
 
-path = 'C:\\...'
-os.chdir(path)
+path = 'C:\\...'        # Absolute path, where the file is located.
+os.chdir(path)          # Change the current working directory to this path.
 
-file = 'somefile.txt'
+file = 'somefile.txt'   # Name of the file, which is to be imported.
+
+# Opening procedure. Instead of printing a line it is also possible to append the 
+# content to a data structure list, dictionary, etc.
 
 with open(file, 'r', encoding='utf-8') as file_input:
     try:
@@ -19,3 +24,5 @@ with open(file, 'r', encoding='utf-8') as file_input:
             print(line, end='')
     except StopIteration:
         pass
+
+file_input.close()      # Close the input file afterwards.
